0c92df37dfabccb512393efe4d49a5e2fdd041d1
Yunzhu Jin <jyz022020@gmail.com>
2022-05-07 16:01:09 -0700

    get user zodiac sign, animal and prediction

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const ho = require('./utils/horoscope');
 const userSign = ho.getUserSign(1,25);
+const userAnimalSign =ho.getUserAnimal(1995);
+
 console.log(userSign);
+console.log(userAnimalSign);
 ho.getPrediction(userSign);
+
 
--- utils/horoscope.js
+++ utils/horoscope.js
@@ -1,10 +1,16 @@
 const getSign = require('horoscope').getSign;
+const getZodiacAnimal = require('chinese-year');
 const getZodiac = require('horoscope').getZodiac;
 const request = require('request');
 
 
 function getUserSign(month,day){
     return getSign({month: month, day: day });
+}
+
+function getUserAnimal(year){
+    const userAnimal = getZodiacAnimal.getAnimal(year);
+    return userAnimal;    
 }
 
 function getPrediction(sign){
@@ -23,3 +29,4 @@ function callback(error, response, body) {
 
 exports.getUserSign = getUserSign;
 exports.getPrediction = getPrediction;
+exports.getUserAnimal= getUserAnimal;
