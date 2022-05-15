5996f69cc548720351d6977f4fe1b3489f71e569
travnichols916 <travnichols916@GMAIL.COM>
2022-05-14 21:19:13 -0700

    alter tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  content: [],
+  content: ['./views/layouts/main.handlebars',
+             './views/partials/horoscope-details.handlebars',
+             './views/partials/painting-details.handlebars',
+             './views/dashboard.handlebars',
+             './views/login.handlebars',
+             './views/horoscope.handlebars',
+],
   theme: {
+    colors: {},
     extend: {},
   },
   plugins: [],
